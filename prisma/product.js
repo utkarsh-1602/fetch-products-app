8644d0590a43a6d14ea77647fbf9924f29e94b6b
utkarsh-1602 <usking619@gmail.com>
2023-11-04 00:42:26 +0530

    added: setup prisma, APIs

---
+++ prisma/product.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const createProduct = async (image, title, price, category) => {
+    const product = await prisma.product.create({
+        data: {
+            image,
+            title,
+            price,
+            category
+        }
+    });
+
+    return product
+}
+
+
+export const getAllProducts = async () => {
+    const products = await prisma.product.findMany();
+
+    return products
+}
+
+
+export const deleteProduct = async (id) => {
+    const products = await prisma.product.delete({
+        where: {
+            id: id
+        }
+    })
+
+    return products
+}
