cacf7aa5ada6f771f4be4a020296eac5c5d02fe4
utkarsh-1602 <usking619@gmail.com>
2023-11-05 01:22:30 +0530

    added : add, get, delete product, realtime caching

--- pages/products/add-product.jsx
+++ pages/products/add-product.jsx
@@ -1,6 +1,7 @@
 import useAddProduct from "@/hooks/useAddProduct"
 import { useRouter } from "next/router"
 import { useState } from "react"
+import axios from "axios"
 
 const addProduct = () => {
 
@@ -8,25 +9,56 @@ const addProduct = () => {
     const router = useRouter();
 
     const [title, setTitle] = useState('')
-    const [image, setImage] = useState('')
+    const [image, setImage] = useState('') // TODO: Add Multer to Upload the Image
     const [price, setPrice] = useState('')
     const [description, setDescription] = useState('')
     const [category, setCategory] = useState('')
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFileName, setSelectedFileName] = useState('');
 
 
-    const handleSubmit = (e) => {
-        console.log(title, image, price, description, category)
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
+        setSelectedFileName(file ? file.name : ''); // Update with the selected file's name.
+    };
+
+
+    const handleSubmit = async (e) => {
+        // console.log(title, image, price, description, category)
+        // e.preventDefault();
+
+        // const newProduct = {
+        //     title: title,
+        //     image: image,
+        //     price: parseFloat(price),
+        //     description: description,
+        //     category: category
+        // }
+        // addProduct(newProduct)
+        // router.push('/api/products')
+        // console.log("Submit")
         e.preventDefault();
-        const newProduct = {
-            title: title,
-            image: image,
-            price: parseFloat(price),
-            description: description,
-            category: category
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('title', title);
+            formData.append('price', price);
+            formData.append('description', description);
+            formData.append('category', category);
+            formData.append('image', selectedFile); // Append the selected file.
+
+            try {
+                // Make a POST request to your add product API.
+                await addProduct(formData);
+                router.push('/api/products');
+                console.log('Submit');
+            } catch (error) {
+                console.error('Error submitting the product', error);
+            }
+        } else {
+            console.error('No file selected');
         }
-        addProduct(newProduct)
-        router.push('/api/products')
-        console.log("Submit")
     }
 
     return (
@@ -34,7 +66,7 @@ const addProduct = () => {
             <div class="flex justify-center items-center h-screen bg-black-900">
                 <div class="max-w-md mx-auto bg-white p-8 rounded shadow-lg lg:w-1/2">
                     <h2 class="text-2xl font-semibold mb-4 text-black ">Add Product 🛍️</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit} encType="multipart/form-data">
                         <div class="mb-4">
                             <label for="title" class="block text-gray-600">Title</label>
                             <input
@@ -47,11 +79,11 @@ const addProduct = () => {
                                 onChange={(e) => setCategory(e.target.value)}
                                 value={category}
                                 id="category" name="category" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-400 text-black bg-purple-100">
-                                <option value="">Select a Category</option>
-                                <option value="option1">Electronics</option>
-                                <option value="option2">Clothes</option>
-                                <option value="option3">Food</option>
-                                <option value="option4">Books</option>
+                                <option >Select a Category</option>
+                                <option >Electronics</option>
+                                <option >Clothes</option>
+                                <option >Food</option>
+                                <option >Books</option>
                             </select>
                         </div>
                         <div class="mb-4">
@@ -68,7 +100,10 @@ const addProduct = () => {
                         </div>
                         <div class="mb-4">
                             <label for="image" class="block text-gray-600">Image</label>
-                            <input type="file" id="image" name="image" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-400 text-black" />
+                            <input
+                                onChange={handleFileChange}
+                                accept=".jpg, .png"
+                                type="file" id="image" name="image" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-400 text-black" />
                         </div>
                         <div class="mt-4">
                             <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600 transform transition-transform duration-300 hover:scale-105">Submit</button>
--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,7 +1,43 @@
+import useDeleteProduct from "@/hooks/useDeleteProduct"
+import useGetProducts from "@/hooks/useGetProducts"
 
 const Products = () => {
+
+    const { data: products, isLoading, error } = useGetProducts()
+    const deleteProduct = useDeleteProduct('/api/products')
+    const handleDeleteProduct = async (id) => {
+        await deleteProduct(id)
+    }
+
     return (
-        <div>Products</div>
+        <>
+
+            <div className="bg-black-900">
+                <div className="container mx-auto p-4">
+                    <h1 className="text-2xl font-semibold text-white mb-4">All Products</h1>
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : error ? (
+                        <p>Error: {error.message}</p>
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                            {products.map(product => (
+                                <div key={product.id} className="bg-white p-4 rounded shadow-lg">
+                                    <img src={product.image} className="w-full h-48 object-cover mb-2 text-black" />
+                                    <h2 className="text-lg font-semibold text-black">{product.title}</h2>
+                                    <p className="text-gray-600">{product.description}</p>
+                                    <p className="text-green-500 font-semibold mt-2">₹ {product.price}</p>
+                                    <p className="text-blue-500 font-semibold mt-2">{product?.category}</p>
+                                    <button
+                                        onClick={() => handleDeleteProduct(product.id)}
+                                        type="button" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 focus:outline-none focus:bg-red-600 transform transition-transform duration-300 hover:scale-105">Remove from cart</button>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
     )
 }
 
